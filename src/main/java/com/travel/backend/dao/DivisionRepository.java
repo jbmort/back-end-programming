c83caab3e29bb739d5bf5f068b5d12661ac494a4
Jacob Mortensen <jmortens@wgu.edu>
2025-02-22 07:33:45 -0800

    Part E Complete

---
+++ src/main/java/com/travel/backend/dao/DivisionRepository.java
@@ -0,0 +1,9 @@
+package com.travel.backend.dao;
+
+import com.travel.backend.entities.Division;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public interface DivisionRepository extends JpaRepository<Division, Long> {
+}
